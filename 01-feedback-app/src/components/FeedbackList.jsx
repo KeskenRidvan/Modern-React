5c486c77cec205ead4bc40d9fcecd86858bb615e
Ridvan Kesken <kesken.dev@gmail.com>
2025-04-16 21:03:16 +0300

    Context API and useContext Hook were covered and chapter 6 was finished.

--- 01-feedback-app/src/components/FeedbackList.jsx
+++ 01-feedback-app/src/components/FeedbackList.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import PropTypes from "prop-types";
+import { React, useContext } from "react";
+
 import { motion, AnimatePresence } from "framer-motion";
 
 import FeedbackItem from "./FeedbackItem";
 import Card from "./shared/Card";
 
-function FeedbackList({ feedback, handleDelete }) {
+import FeedbackContext from "../context/FeedbackContext";
+
+function FeedbackList() {
+  const { feedback } = useContext(FeedbackContext);
+
   if (!feedback || feedback.length === 0) {
     return (
       <Card>
@@ -25,26 +29,12 @@ function FeedbackList({ feedback, handleDelete }) {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            <FeedbackItem
-              key={item.id}
-              item={item}
-              handleDelete={handleDelete}
-            />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
     </div>
   );
 }
-
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  ),
-};
 
 export default FeedbackList;
